20a532aefd801712a8d2d5a9a2f4efdf322bdd5c
floraisondev <devanshi07.tank@gmail.com>
2021-07-11 18:58:54 +0530

    added contact fixed gallery

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -3,16 +3,16 @@ import React, {useEffect, useState} from "react"
 import "../css/main.css"
 import { SRLWrapper } from "simple-react-lightbox" 
 const images = [
-    {id : '1', imageName : <StaticImage src="./gallery-images/img1.jpg"></StaticImage>, src : "./gallery-images/img1.jpg",  tag : "temp"},
-    {id : '2', imageName :  <StaticImage src="./gallery-images/img2.jpg"></StaticImage>, src : "./gallery-images/img2.jpg", tag : "perm"},
-    {id : '3', imageName :  <StaticImage src="./gallery-images/img3.jpg"></StaticImage>, src : "./gallery-images/img3.jpg", tag : "temp"},
-    {id : '4', imageName :  <StaticImage src="./gallery-images/img4.jpg"></StaticImage>, src : "./gallery-images/img4.jpg", tag : "temp"},
-    {id : '5', imageName :  <StaticImage src="./gallery-images/img5.jpg"></StaticImage>, src : "./gallery-images/img5.jpg", tag : "perm"},
-    {id : '6', imageName :  <StaticImage src="./gallery-images/img6.jpg"></StaticImage>, src : "./gallery-images/img6.jpg", tag : "temp"},
-    {id : '7', imageName :  <StaticImage src="./gallery-images/img7.jpg"></StaticImage>, src : "./gallery-images/img7.jpg", tag : "temp"},
-    {id : '8', imageName :  <StaticImage src="./gallery-images/img8.jpg"></StaticImage>, src : "./gallery-images/img8.jpg", tag : "celeb"},
-    {id : '9', imageName :  <StaticImage src="./gallery-images/img9.jpg"></StaticImage>, src : "./gallery-images/img9.jpg", tag : "temp"},
-    {id : '10', imageName :  <StaticImage src="./gallery-images/img10.jpg"></StaticImage>,src : "./gallery-images/img10.jpg", tag : "celeb"},
+    {id : '1', imageName : <StaticImage src="./gallery-images/img1.jpg" height = {300} width = {300}></StaticImage>, src : "./gallery-images/img1.jpg",  tag : "temp"},
+    {id : '2', imageName :  <StaticImage src="./gallery-images/img2.jpg"  height = {300} width = {300}></StaticImage>, src : "./gallery-images/img2.jpg", tag : "perm"},
+    {id : '3', imageName :  <StaticImage src="./gallery-images/img3.jpg" height = {300} width = {300}></StaticImage>, src : "./gallery-images/img3.jpg", tag : "temp"},
+    {id : '4', imageName :  <StaticImage src="./gallery-images/img4.jpg" height = {300} width = {300}></StaticImage>, src : "./gallery-images/img4.jpg", tag : "temp"},
+    {id : '5', imageName :  <StaticImage src="./gallery-images/img5.jpg" height = {300} width = {300}></StaticImage>, src : "./gallery-images/img5.jpg", tag : "perm"},
+    {id : '6', imageName :  <StaticImage src="./gallery-images/img6.jpg" height = {300} width = {300}></StaticImage>, src : "./gallery-images/img6.jpg", tag : "temp"},
+    {id : '7', imageName :  <StaticImage src="./gallery-images/img7.jpg" height = {300} width = {300}></StaticImage>, src : "./gallery-images/img7.jpg", tag : "temp"},
+    {id : '8', imageName :  <StaticImage src="./gallery-images/img8.jpg" height = {300} width = {300}></StaticImage>, src : "./gallery-images/img8.jpg", tag : "celeb"},
+    {id : '9', imageName :  <StaticImage src="./gallery-images/img9.jpg" height = {300} width = {300}></StaticImage>, src : "./gallery-images/img9.jpg", tag : "temp"},
+    {id : '10', imageName :  <StaticImage src="./gallery-images/img10.jpg" height = {300} width = {300}></StaticImage>,src : "./gallery-images/img10.jpg", tag : "celeb"},
 ]
 const Gallery = () => {
     const [tag , setTag] = useState('all')
@@ -25,10 +25,11 @@ useEffect(
 )
 return (
     <div style = {{
-        background : "#000",
+        background : "linear-gradient(239.94deg, #201f1f -12.77%, #000000 102.42%)",
         height : "100vh",
+        display : "flex",
         overflowY : "scroll",
-        display : "flex"
+        flexDirection : "column"
     }}>
     <div className="tags">
     <TagButton name = "all" handleSetTag = {setTag}/>
@@ -36,7 +37,10 @@ return (
     <TagButton name = "perm" handleSetTag = {setTag}/>
     <TagButton name = "celeb" handleSetTag = {setTag}/>
     </div>
-   <SRLWrapper>
+    <div>
+        
+    </div>
+   <SRLWrapper> 
     <div className="container">
    {
      filteredImages.map(image => 
@@ -52,6 +56,6 @@ return (
 }
 
 const TagButton = ({name, handleSetTag}) => {
-return <button className = "tag book-now-btn" onClick={()=> handleSetTag(name)}>{name.toUpperCase()}</button>
+return <button className = "tag" onClick={()=> handleSetTag(name)}>{name.toUpperCase()}</button>
 }
 export default Gallery
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,8 +5,24 @@ const Contact = () => {
  
     return(
   <Layout>
- <h1>About</h1>
-    </Layout>
+<section style = {{ height : "70vh", background : "#000"}}>
+ <h1 className= "title-class" style={{ textAlign: "center"}}>Contact</h1>
+ <div style = {{ display : "flex", justifyContent : "space-evenly"}}>
+   <div>
+     <h2 className= "title-class">Studio Address</h2>
+     <p>205, Block 5, Skyline City Appt</p>
+     <p>Chandralyout, Bengaluru. 560073</p>
+     <p>Contact : +(91) 982564322</p>
+   </div>
+   <div>
+   <h2 className= "title-class">Permanent Adress</h2>
+     <p>205, Block 5, Skyline City Appt</p>
+     <p>Chandralyout, Bengaluru. 560073</p>
+     <p>Contact : +(91) 982564322</p>
+   </div>
+ </div>
+ </section>
+</Layout>
     )
 }
 export default Contact
